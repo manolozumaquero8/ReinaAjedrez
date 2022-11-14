d25dfe4db83bfa47e26b8eee9348d5ed55968574
usuario <usuario@DESKTOP-9JLF6U0>
2022-11-14 12:21:00 +0100

    Direccion

---
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -0,0 +1,74 @@
+package org.iesalandalus.programacion.reinaajedrez.modelo;
+
+public class Posicion {
+
+	private static final int FILA_X = 1 ;
+	private static final int COLUMNA_X = ;
+
+	private int x;
+	private int y;
+
+	public Posicion() {
+		x = FILA_X;
+		y = FILA_Y;
+	}
+
+	public Posicion(int x, int y) {
+		setX(x);
+		setY(y);
+	}
+
+	public Posicion(Posicion posicion) {
+		if (posicion == null) {
+			throw new NullPointerException("No puedo copiar una posición nula.");
+		}
+		setX(posicion.getX());
+		setY(posicion.getY());
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		if (x < FILA_X) {
+			throw new IllegalArgumentException("El valor de la x es menor que el mínimo permitido.");
+		} else if (x > COLUMNA_X) {
+			throw new IllegalArgumentException("El valor de la x es mayor que el máximo permitido.");
+		}
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		if (y < FILA_Y) {
+			throw new IllegalArgumentException("El valor de la y es menor que el mínimo permitido.");
+		} else if (y > COLUMNA_Y) {
+			throw new IllegalArgumentException("El valor de la y es mayor que el máximo permitido.");
+		}
+		this.y = y;
+	}
+
+	public int hashCode() {
+		return Objects.hash(x, y);
+	}
+
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Posicion other = (Posicion) obj;
+		return x == other.x && y == other.y;
+	}
+
+	public String toString() {
+		return String.format("x=%s, y=%s", x, y);
+	}
+
+}
